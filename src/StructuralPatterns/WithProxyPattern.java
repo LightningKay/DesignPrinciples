ea99e56d9e9180562e0b5727e6ad47bd0c34df04
Karthik K <32917045+LightningKay@users.noreply.github.com>
2025-10-11 17:18:13 -0700

    First Commit;

---
+++ src/StructuralPatterns/WithProxyPattern.java
@@ -0,0 +1,64 @@
+package StructuralPatterns;
+
+import java.util.HashMap;
+
+interface VideoDownloader1{
+    String downloadVideo(String videoUrl);
+}
+
+class DownloadVideo{
+    public DownloadVideo(){
+    }
+    public String downloadVideo(String name){
+
+        System.out.println("Downloading Video - " + name);
+        String returnVideo =  "Downloaded Video - " + name;
+        return returnVideo;
+    }
+}
+
+class CachedDownloadVideo implements VideoDownloader1{
+
+    HashMap<String, String> cache;
+    public CachedDownloadVideo(){
+        cache = new HashMap<>();
+    }
+
+
+    @Override
+    public String downloadVideo(String videoUrl) {
+        if(cache.containsKey(videoUrl)){
+            System.out.println("Downloading from cache");
+            return cache.get(videoUrl);
+        }
+
+        DownloadVideo downloadVideo = new DownloadVideo();
+        String video =  downloadVideo.downloadVideo(videoUrl);
+        cache.put(videoUrl, video);
+        return video;
+    }
+}
+
+class SingletonFactory1{
+
+    static CachedDownloadVideo cachedDownloadVideo;
+
+    public static CachedDownloadVideo getCachedDownloadVideo(){
+        if(cachedDownloadVideo == null){
+            cachedDownloadVideo = new CachedDownloadVideo();
+            return cachedDownloadVideo;
+        }
+
+        return cachedDownloadVideo;
+    }
+}
+public class WithProxyPattern {
+    public static void main(String[] args) {
+        VideoDownloader1 videoDownloader1 = SingletonFactory1.getCachedDownloadVideo();
+        videoDownloader1.downloadVideo("Video 1");
+
+        VideoDownloader1 videoDownloader2 = SingletonFactory1.getCachedDownloadVideo();
+        System.out.println(videoDownloader1 + " " + videoDownloader2);
+        videoDownloader2.downloadVideo("Video 1");
+    }
+}
--- src/StructuralPatterns/WithoutProxyPattern.java
+++ src/StructuralPatterns/WithoutProxyPattern.java
@@ -1,4 +1,50 @@
 package StructuralPatterns;
+import java.util.*;
 
+class VideoDownloader{
+
+    static HashMap<String, String> cache;
+    public VideoDownloader(){
+        cache = new HashMap<>();
+    }
+    public String downloadVideo(String name){
+
+        if(cache.containsKey(name)){
+            System.out.println("Returning from cache");
+            return cache.get(name);
+        }
+        // caching logic
+
+
+        System.out.println("Downloading Video - " + name);
+        String returnVideo =  "Downloaded Video - " + name;
+        cache.put(name, returnVideo);
+        return returnVideo;
+    }
+}
+
+class SingletonFactory{
+    static VideoDownloader videoDownloader;
+
+    public SingletonFactory(){
+
+    }
+
+    public static synchronized VideoDownloader getVideoDownloader() {
+        if(videoDownloader == null){
+            videoDownloader =  new VideoDownloader();
+            return videoDownloader;
+        }
+        return videoDownloader;
+    }
+}
 public class WithoutProxyPattern {
+
+    public static void main(String[] args) {
+        VideoDownloader downloader = SingletonFactory.getVideoDownloader();
+        downloader.downloadVideo("new video");
+
+        VideoDownloader downloaderCache= SingletonFactory.getVideoDownloader();
+        downloaderCache.downloadVideo("new video");
+    }
 }
