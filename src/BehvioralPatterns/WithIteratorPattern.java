eefecf7f83df6fa245f8973bd4bd627f9bcf29b1
Karthik K <32917045+LightningKay@users.noreply.github.com>
2025-10-11 19:35:12 -0700

    Iterator Pattern

---
+++ src/BehvioralPatterns/WithIteratorPattern.java
@@ -0,0 +1,82 @@
+package BehvioralPatterns;
+import java.util.*;
+
+
+class Video1{
+    String name;
+    static long videoInc = 1l;
+    long currVideoId;
+
+    public Video1(String name){
+        this.name = name;
+        this.currVideoId = videoInc++;
+    }
+
+    public long getVideoId(){
+        return currVideoId;
+    }
+}
+
+interface PlaylistIterator{
+    boolean hasNext();
+    Video1 next();
+}
+
+class VideoIterator implements PlaylistIterator{
+
+    List<Video1> playlist;
+
+    int count;
+    public VideoIterator(List<Video1> playlist){
+        this.playlist = playlist;
+    }
+
+    public boolean hasNext(){
+        return count != playlist.size();
+    }
+
+    public Video1 next(){
+        return hasNext() ? playlist.get(count++) : null;
+    }
+
+}
+class VideoPlaylist1{
+    String playlistName;
+    private List<Video1> videos;
+
+    public VideoPlaylist1(String playlistName){
+        this.playlistName = playlistName;
+        videos = new ArrayList<>();
+    }
+
+    public void addVideo(Video1 v){
+        videos.add(v);
+    }
+
+    public VideoIterator iterator(){
+        return new VideoIterator(videos);
+    }
+}
+public class WithIteratorPattern {
+
+    public static void main(String[] args) {
+        Video1 v1 = new Video1("v1");
+        Video1 v2 = new Video1("v2");
+        Video1 v3 = new Video1("v3");
+        Video1 v4 = new Video1("v4");
+
+        VideoPlaylist1 playlist = new VideoPlaylist1("My playlist");
+        playlist.addVideo(v1);
+        playlist.addVideo(v2);
+        playlist.addVideo(v3);
+        playlist.addVideo(v4);
+
+        VideoIterator iter = playlist.iterator();
+
+        while(iter.hasNext()){
+            Video1 v = iter.next();
+            System.out.println(v.getVideoId());
+        }
+
+    }
+}
---
+++ src/BehvioralPatterns/WithoutIteratorPattern.java
@@ -0,0 +1,58 @@
+package BehvioralPatterns;
+
+import java.util.*;
+
+class Video {
+    String name;
+    static long videoId = 1L;
+    long currVideoId;
+    public Video(String name){
+        this.name = name;
+        this.currVideoId = videoId++;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public long getVideoId(){
+        return currVideoId;
+    }
+}
+
+class VideoPlaylist{
+    String playlistName;
+    List<Video> playlist;
+
+    public VideoPlaylist(String playlistName){
+        this.playlistName = playlistName;
+        playlist = new ArrayList<>();
+    }
+
+    public String getPlaylistName(){
+        return playlistName;
+    }
+
+    public void addVideo(Video videoToAdd){
+        this.playlist.add(videoToAdd);
+    }
+}
+public class WithoutIteratorPattern {
+
+    public static void main(String[] args) {
+        Video v1 = new Video("video1");
+        Video v2 = new Video("video2");
+        Video v3 = new Video("video3");
+
+        VideoPlaylist playlist = new VideoPlaylist("New playlist");
+        playlist.addVideo(v1);
+        playlist.addVideo(v2);
+        playlist.addVideo(v3);
+
+        for(Video v : playlist.playlist){
+            System.out.println(v.getName() + " " + v.getVideoId());
+        }
+
+
+    }
+}
